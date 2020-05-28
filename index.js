e1f743c948f39b9fed2a6b0e53d68bd41a0dedb9
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-28 14:33:30 -0700

    word count problem

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 
+function countWords(sentence) {
+  const words = sentence.toLowerCase().replace(/[^A-Za-z0-9 ]/g, '').split(' ');
 
-function main() {
+  const count = {}
 
+  words.forEach(word => {
+    if(count[word]) {
+      count[word]++
+    }
+    else {
+      count[word] = 1
+    }
+  })
+
+  let result = ''
+  Object.entries(count).forEach(pair => {
+    result += `${pair[0]} = ${pair[1]}, `
+  })
+
+  console.log(result)
+
+  return words
 }
 
+
+function main() {
+  const sentence = "Hello there, how are you? Can you tell me how to get to the nearest Starbucks?"
+  countWords(sentence)
+
+  
+}
 main()
